81909e089c3520658ebd18ef791a5f4688b4188c
marvinick <marviluw@gmail.com>
2014-02-01 17:54:08 -0600

    resources and posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def show
+    @post = Post.find(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+  resources :posts, except: [:destroy]
 end
