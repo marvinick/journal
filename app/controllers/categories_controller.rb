9a7fd96c0660f1a9d2bb1790933897ac8d6e3214
marvinick <marviluw@gmail.com>
2014-02-04 08:39:43 -0600

    started categorie

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,31 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category has been created"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   resources :posts, except: [:destroy]
+  resources :categories, only: [:new, :show, :create]
 end
