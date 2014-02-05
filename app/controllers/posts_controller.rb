9ea309f98b1f001da7aa0ced1b10aee82d9d18a2
marvinick <marviluw@gmail.com>
2014-02-05 17:57:38 -0600

    category_ids virtual attributes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :url)
+    params.require(:post).permit(:title, :description, :url, category_ids: [])
   end
 
   def set_post
