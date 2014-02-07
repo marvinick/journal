1e870d3a41e5d1a0730ab1eaea571bfaeb413bd5
marvinick <marviluw@gmail.com>
2014-02-07 14:40:25 -0600

    adding register new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
 
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create,  length: {minimum: 5}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
@@ -9,5 +10,7 @@ PostitTemplate::Application.routes.draw do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :show, :create]
+
+  resources :users, only: [:show, :create, :edit, :update]
 
 end
